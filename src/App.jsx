38ea7d1dd057d2d5e98b8b7f51b7fd4b94edaa6e
Srihari <99220041874@klu.ac.in>
2026-07-06 18:01:23 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ScrollTrigger, SplitText} from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
+const App = () => {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-blue-400">
+        Hello Gsap
+      </h1>
+    </div>
+  )
+}
+
+export default App
